ca2ea60a781afba80a66f84969c9fd9fba0902f5
Kev Bonett <KBonett@omnicogroup.com>
2019-08-21 16:13:31 +0100

    add docspage

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
 import { configure } from '@storybook/react';
+import { addParameters } from '@storybook/react';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
+
+addParameters({
+  docsContainer: DocsContainer,
+  docs: DocsPage,
+});
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.js$/), module);
